970eeb48329bd5f4ca49f907659a2f943e40f221
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-08-29 13:24:46 -0500

    failing at 15, but overall working well to scrape and dump metadata with mp3s

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ from pprint import pprint
 from json import dump
 from collections import defaultdict
 
+
 class Episode:
     def __init__(episode, entry, certificate, release_date, title, mp3):
         self.episode = episode
@@ -20,16 +21,20 @@ class Episode:
         return " "
 
 def make_soup(url):
+    logger.debug("entering soup")
     return BeautifulSoup(requests.get(url).text, "html.parser").body
 
-def ep_info(soup):
+def get_ep_info(soup):
+    logger.debug("entering ep_info")
     return soup.find("div", class_="hero-info")
 
-def desc_info(soup):
-    return soup.find("section", class_="split").div.p.p.text
+def get_desc_info(soup):
+    logger.debug("desc_info")
+    desc = soup.find("div", class_="split-primary prose").find_all("p")
+    return "".join([p.text for p in desc])
 
 def get_title(ep_info):
-    return ep_info.find("h1").text
+    return ep_info.h1.text
 
 def get_date(ep_info):
     return ep_info.find("i", class_="fas fa-calendar-alt").parent.text.strip()
@@ -50,24 +55,41 @@ def get_certificate(desc):
     return re.search(search_string, desc).group(1)
 
 if __name__ == "__main__":
+    import time
+    import logging
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler("scrape.log")
+    logger.addHandler(file_handler)
+    
 
     episodes = defaultdict(dict)
     URI = "https://www.omnibusproject.com/"
 
-    for i in range(2):
-        url = f"{URI}{str(i)}"
-        soup = make_soup(url)
-        
-        ep_info = ep_info(soup)
-        desc_info = desc_info(soup)
+    episode_number=1
+    while episode_number != 0:
+        logger.debug(episode_number)
+        url = f"{URI}{str(episode_number)}"
+        logger.debug(url)
+        try:
+            soup = make_soup(url)
+        except:
+            episode_number=0
+        logger.debug("leaving soup")
+        ep_info = get_ep_info(soup)
+        desc_info = get_desc_info(soup)
         title = get_title(ep_info)
         
-        episodes[i] = {"date":get_date(ep_info), "entry":get_entry(title), "title":get_title_desc(title), "certificate": get_certificate(desc_info), "mp3":get_mp3(ep_info)}
+        episodes[episode_number] = {"date":get_date(ep_info), "entry":get_entry(title), "title":get_title_desc(title), "certificate": get_certificate(desc_info), "mp3":get_mp3(ep_info)}
+        
+        episode_number+=1
+        logger.debug("restarting loop...")
+        
 
     with open("episodes.json", "w") as f:
         dump(episodes, f)
         
     pprint(episodes)
 
-        
 
+    
